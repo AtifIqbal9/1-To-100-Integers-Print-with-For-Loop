a5d343b913c4a823f253154726035234f0fadbfb
AtifIqbal9 <srkatif1199@gmail.com>
2022-09-08 00:19:22 +0500

    done

---
+++ int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int i; 
+
+    for (int i = 0; i <= 100; i++) //  “<=” means “less than or equal to”
+    {
+       printf("%d", i);
+
+    }
+    
+
+return 0;
+}
